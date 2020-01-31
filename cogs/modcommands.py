d8cc939afb41653d71bbef47a1ac6b68aac18a6b
Lach993 <30387716+Lach993@users.noreply.github.com>
2020-01-31 11:41:48 +0800

    Added Ban + fixed syntax + added descriptions
    Added the ban command
    fixed syntax of the mute and kick command:
        fixed the decorator from @bot.command to @commands.command(name = "name")
        replaced "bot" with "self.bot"
        added basic descriptions  for all functions

--- cogs/modcommands.py
+++ cogs/modcommands.py
@@ -15,19 +15,49 @@ class ModCommands(commands.Cog):
         self.bot = bot
         self.suggest_channel_id = 672135121469571092
 
-    @bot.command
-    async def kick(ctx, member: discord.Member):
-        role = discord.utils.get(ctx.guild.roles, name="Support")
+    @commands.command(name = "kick")
+    async def kick(self, ctx, member: discord.Member):
+        """Kicks a user based on a mention"""
+        role = discord.utils.get(ctx.guild.roles, name = "Support")
         if role in ctx.author.roles:
-            await bot.kick(member)
+            await self.bot.kick(member)
     
-    @bot.command
-    async def mute(ctx, member: discord.Member):
-        role = discord.utils.get(ctx.guild.roles, name="Support")
+    @commands.command(name = "mute")
+    async def mute(self, ctx, member: discord.Member):
+        """mutes a user based on a mention"""
+        role = discord.utils.get(ctx.guild.roles, name = "Support")
         if role in ctx.author.roles:
-            muted = discord.utils.get(member.guild.roles, name="Muted")
-            await bot.add_roles(member, muted)
+            muted = discord.utils.get(member.guild.roles, name = "Muted")
+            await self.bot.add_roles(member, muted)
             await ctx.send("User muted")
+   
+    @commands.command(name = "ban")
+    async def ban(self, ctx, locator: str = None, *, reason: str = None) 
+        """Bans a user from the guild
+           locator can be the following message types:
+                Mention      - @Persons name
+                ID           - 1234568765473
+                name#discrim - Person12#1234
+           Usage:
+                ban [locator] {OPTIONAL: reason can be multiple words}
+                ban 2347832428 He was disrespecting staff
+                ban idk#4567 
+        """
+                
+        try:
+            user = await commands.UserConverter().convert(ctx, locator)
+            # send a message to the user explaining they have been banned and how to get unbanned
+            await user.send(f"""Hi you have been banned from {ctx.guild.name}.
+                                if you would like to come back. please fill out this forum.
+                                https://forms.gle/dCLv2QZq5LHdyTuL8""")
+            # ban the user from the guild. NOTE THIS IS SET TO NOT DELETE MESSAGES. 
+            # REMOVE "delete_messages_days = 0" OR CHANGE VALUE TO DELETE MESSAGES!
+            await ctx.guild.ban(user, reason, delete_message_days = 0)
+            await ctx.send(f"the ban hammer has spoken:\n\t{user} has been banned.")
+
+        # if the bot cannot find the user tell the person in chat.
+        except commands.errors.BadArgument:
+            await ctx.send(f"The user you where looking for {locator} could not be found.")
             
 
 def setup(bot):
