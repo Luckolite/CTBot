faa9c7d7fc53b19c9cb85881f2a55811c3b91752
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-01-31 08:55:49 -0500

    added more activities

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ errors = []
 async def status_task():
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name='Back Online'))
     while True:
-        activities = ['With Crafting Tables']
+        activities = ['In The Valley of Crafting Tables', 'With Tools', '2B2T']
         for activity in activities:
             await asyncio.sleep(15)
             await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=activity))
