73415c002b9d5168d8104d1b7f2d214f639f73f7
FrequencyX4 <FrequencyX4@users.noreply.github.com>
2020-02-03 20:03:07 -0500

    added utils/utils.py

---
+++ utils/utils.py
@@ -0,0 +1,10 @@
+def bytes2human(n):
+	symbols = ('kb', 'mb', 'gb', 'tb', 'pb', 'E', 'Z', 'Y')
+	prefix = {}
+	for i, s in enumerate(symbols):
+		prefix[s] = 1 << (i + 1) * 10
+	for s in reversed(symbols):
+		if n >= prefix[s]:
+			value = float(n) / prefix[s]
+			return '%.1f%s' % (value, s)
+	return "%sB" % n
