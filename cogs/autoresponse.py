b94f33c62f9c76fd990c90a0436cf1dc1efa3320
mouncg <ert@danwin1210.me>
2020-02-08 18:26:09 +0000

    added comments to autoresponse

--- cogs/autoresponse.py
+++ cogs/autoresponse.py
@@ -1,23 +1,23 @@
-from discord.ext import commands
+from discord.ext import commands  # imports
 
 
-class AutoResponse(commands.Cog):
-    def __init__(self, bot):
+class AutoResponse(commands.Cog):  # the class
+    def __init__(self, bot):  # go look at appeals for more advanced definitions.
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if not message.author.bot:
-            words = message.content.lower().split()
-            statements = {
+    @commands.Cog.listener()  # listener
+    async def on_message(self, message):  # on message
+        if not message.author.bot:  # if the message author is not a bot
+            words = message.content.lower().split()  # gets the words in the message.
+            statements = {  # the statements in json format
                 "furnace": "crafting table is obviously the superior block",
                 "fit": "who dat",
                 "@everyone": "<:ping:623235635897171999>"
             }
 
-            for item in statements:
-                if item in words:
-                    await message.channel.send(statements[item])
+            for item in statements:  # self explanetory
+                if item in words:  # self explanetory
+                    await message.channel.send(statements[item])  # easy to read
 
 
 def setup(bot):
