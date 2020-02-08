b64c6eab46663c6ab19982a1615b81d79f803746
Lach993 <30387716+Lach993@users.noreply.github.com>
2020-02-08 14:51:48 +0800

    made it a bit more flexible
    added .lower() to message.content to make caps not matter
    added a dictionary containing the statements so if we want to add more later its not a big deal to add like 3 lines per message.
    --end--
    
    now for another one of my commit changes where i tell my life story: i haven't programmed in like 8 days cause school is getting harder and i don't have time, its actually really disappointing and i noticed that the amount of things i have forgotten is INSANE. like im second judging myself if i even wrote that correctly or if the syntax is just 100% wrong. idek at this point. anyway i need to learn to put some time aside so i can do more projects and mess around with python again.

--- cogs/autoresponse.py
+++ cogs/autoresponse.py
@@ -8,14 +8,17 @@ class AutoResponse(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
-            words = message.content.split()
-            if "furnace" in words:
-                await message.channel.send("crafting table is obviously the superior block")
-            elif "fit" in words:
-                await message.channel.send("who dat")
-            elif "@everyone" in message.content:
-                await message.channel.send("<:ping:623235635897171999>")
-
+            words = message.content.lower().split()
+            statements = {
+                "furnace": "crafting table is obviously the superior block",
+                "fit": "who dat",
+                "@everyone": "<:ping:623235635897171999>"
+            }
+            
+            for item in statements:
+                if item in words:
+                    await message.channel.send(statements[item])
+            
 
 def setup(bot):
     bot.add_cog(AutoResponse(bot))
