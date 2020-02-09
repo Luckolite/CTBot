0a074bf9f090b1e053a5c1bd75697fac7fbbdf28
korochun <gkorochun@gmail.com>
2020-02-09 16:00:24 +0300

    PyMine can almost do a handshake (JsonDecodeError for some reason)

---
+++ pymine/status.py
@@ -0,0 +1,34 @@
+from pymine import packet
+from pymine.types import Long, String
+
+
+class StatusRequestPacket(packet.Packet):
+    id = 0
+    contents = {}
+
+
+class StatusResponsePacket(packet.Packet):
+    id = 0
+    contents = {
+        'response': String
+    }
+
+
+class PingPacket(packet.Packet):
+    id = 1
+    contents = {
+        'payload': Long
+    }
+
+
+class PongPacket(packet.Packet):
+    id = 1
+    contents = {
+        'payload': Long
+    }
+
+
+__packets__ = {
+    0: StatusResponsePacket,
+    1: PongPacket
+}
