64ebaf5ffaf26ff6150d963d2fdc1149d4330fa3
ProgrammerPlays <daddyandwilliam@gmail.com>
2020-02-12 07:34:48 -0800

    Fixed buggy af leveling system

--- cogs/levels.py
+++ cogs/levels.py
@@ -13,12 +13,15 @@ class Levels(commands.Cog):
         if not message.author.bot and randomlint == 5:
             user_id = str(message.author.id)
             if user_id not in self.bot.xpdb:
-                self.bot.xpdb[user.id] = 0
+                self.bot.xpdb[user_id] = 0
             self.bot.xpdb[user_id] += 1
+            
+            if user_id not in self.bot.ldb:
+                self.bot.ldb[user_id] = 0
 
             lxp = (self.bot.ldb[user_id] + 1) * (100 + (2 ** (self.bot.ldb[user_id] + 1)))
             if self.bot.xpdb[user.id] >= lxp:
-                self.bot.ldb += 1
+                self.bot.ldb[user_id] += 1
                 await message.channel.send(message.author.id + ", you leveled up to " + str(self.bot.ldb[user_id]))
             self.bot.save_ldb()
             self.bot.save_xpdb()
