dc09d4c96e1d19ec12e885b43a0ee3755d5ec06f
EPFFORCE <epfforce@mail.epfforce.computer>
2020-02-18 18:53:46 +0100

    try with other systems

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import asyncio
 import json
-import sentry_sdk
-with open('config/config.json') as f:
-    dat = json.load(f)
+from discord_sentry_reporting import use_sentry
+
+
+
 
 # if dat["sentry_dsn"] != "nO":
 #     sentry_sdk.init(dat["sentry_dsn"])
-if not dat["sentry_dsn"] or dat["sentry_dsn"] != "nO":
-    sentry_sdk.init(str(dat["sentry_dsn"]))
+
 import sys
 import traceback
 from datetime import datetime
@@ -153,6 +153,15 @@ async def on_ready():
 
 
 def main():
+    with open('config/config.json') as f:
+        dat = json.load(f)
+    if not dat["sentry_dsn"] or dat["sentry_dsn"] != "nO":
+        use_sentry(
+            bot,  # it is typically named client or bot
+            dsn=str(dat["sentry_dsn"])
+            # put in any sentry keyword arguments (**kwargs) here
+        )
+
     initial_extensions = [
         "cogs.appeals",
         "cogs.autoresponse",
