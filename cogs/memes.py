86f1218ea3e77d5cd5327822d0db9bdbc996c0f3
ProgrammerPlays <daddyandwilliam@gmail.com>
2020-02-19 14:22:45 -0800

    fix

--- cogs/memes.py
+++ cogs/memes.py
@@ -10,22 +10,25 @@ class Memes(commands.Cog):
 
     @commands.command(description="Get a meme")
     async def meme(self, ctx):
-        sub = randint(0, 4)
-        if sub is 0:
-            gs = 'memes'
-        elif sub is 1:
-            gs = 'dankmemes'
-        elif sub is 2:
-            gs = 'deepfriedmemes'
-        elif sub is 3:
-            gs = 'specialsnowflake'
-        elif sub is 4:
-            gs = 'imtoriginals'
-        r = praw.Reddit(user_agent='example')
+        try:
+            sub = randint(0, 4)
+            if sub is 0:
+                gs = 'memes'
+            elif sub is 1:
+                gs = 'dankmemes'
+            elif sub is 2:
+                gs = 'deepfriedmemes'
+            elif sub is 3:
+                gs = 'specialsnowflake'
+            elif sub is 4:
+                gs = 'imtoriginals'
+            r = praw.Reddit(user_agent='example')
         
-        rs = r.get_subreddit(gs)
-        posts = rs.get_random_submission()
-        ctx.send(posts.url)
+            rs = r.get_subreddit(gs)
+            posts = rs.get_random_submission()
+            ctx.send(posts.url)
+        except Exception as e:
+            print(str(e))
 
 
 def setup(bot):
