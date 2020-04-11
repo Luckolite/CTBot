4a60ad677dc9a76b2dd3e6f7a3ab966c31b62e9b
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-04-11 09:43:08 +0100

    fix autoresponse.py *hopefully*

--- cogs/autoresponse.py
+++ cogs/autoresponse.py
@@ -2,26 +2,22 @@ import re
 
 import discord
 from discord.ext import commands
+from main import CTBot
 
 
 class AutoResponse(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: CTBot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         """Checks if the message contains an AutoResponse trigger."""
         if not message.author.bot and message.channel:
-            statements = {
-                "furnace": "crafting table is obviously the superior block",
-                "fit": "who dat",
-                "@everyone": "<:ping:623235635897171999>",
-                "kek": "now thats a bruh moment",
-            }
+            statements = self.bot.config["statements"]  # type: dict
 
             for item in statements:
-                if str(message.content).count(item) > 0:
-                    await message.channel.send(statements[item])
+                if item in str(message.content):
+                    await message.channel.send(f"{statements.get(item)}")
 
 
 def setup(bot):
